0d9789052f065c827c374b96b4a8f1d99d305ddf
ThynkRahul <thynk.rahul@gmail.com>
2025-11-03 00:54:46 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import Landing from "../components/Landing";
 import CanonicalURL from "../components/CanonicalURL";
-import NavBar from "../components/NavBar"
+import NavBar from "../components/Navbar"
 import Footer from "../components/Footer"
 import { Metadata } from "next";
 
